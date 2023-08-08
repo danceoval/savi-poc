4d5ef297924a39ad2508d89d884cbb1419ce86b9
Dan Sohval <daniel.sohval@gmail.com>
2023-08-08 11:38:35 -0400

    prompts chatgpt

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-require('dotenv').config()
-console.log(process.env.API_KEY)
+const {Configuration, OpenAIApi} = require("openai");
+require("dotenv").config()
 
-console.log("Hello World")
+const readline = require('readline');
+
+
+
+const configuration = new Configuration({
+    apiKey: process.env.API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+
+async function getCompletion(content){
+	let messages = [{role: "user", content}];
+	let model = "gpt-3.5-turbo";
+	try {
+        const completion = await openai.createChatCompletion({
+            model,
+            messages
+        })
+        return completion.data.choices[0].message.content
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+async function main(){
+	console.log("*** STARTING ***")
+	prompt = "Meow like a cat";
+	chat = await getCompletion(prompt)
+	console.log(chat)
+	console.log("*** OVER ***")
+}
+
+main()
