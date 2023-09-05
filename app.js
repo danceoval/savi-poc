d44936faa4371df38d481683b9cb15329fe793c2
Dan Sohval <daniel.sohval@gmail.com>
2023-09-05 11:41:31 -0400

    adds/updates style tweaks for msgs

--- app.js
+++ app.js
@@ -16,48 +16,57 @@ const PORT = process.env.PORT || 3000;
 const hardcodedResponse = `
         You should use Vertex AI to perform sentiment analysis on school observation and quarterly student survey data.
         
-        Step 1: Data Collection and Preparation
+        
+        Step 1: Data Collection and Preparation</bold>
         Gather school observation data and quarterly student survey data from all relevant sources, ensuring the data is accurate and up-to-date.
         Preprocess the data to remove any inconsistencies, missing values, and outliers that could skew the analysis.
         Organize the data into a structured format that can be easily analyzed, such as a spreadsheet.
         Learn more about data preparation for sentiment analysis here
 
+        
         Step 2: Sentiment Analysis
         Perform sentiment analysis on the student survey data to quantify the positive and negative sentiments associated with different teaching methods
         Learn about sentiment analysis here
 
+        
         Step 3: Identification of Best Practices 
-        Based on the analysis, pinpoint teaching methods that consistently resonate most positively with students.
-        Collaborate with education experts and instructional designers, if necessary, to validate the identified best practices.
-        Document these best practices in a comprehensive playbook, including explanations, examples, and implementation guidelines.
+        Based on the analysis, pinpoint teaching methods that consistently resonate most positively with students
+        Collaborate with education experts and instructional designers, if necessary, to validate the identified best practices
+        Document these best practices in a comprehensive playbook, including explanations, examples, and implementation guidelines
 
+        
         Step 4: Playbook Creation and Distribution 
         Compile the best practices and insights into a well-structured playbook format.
-        Ensure the playbook is easily accessible and user-friendly, considering digital formats (PDFs, web pages, etc.) for distribution.
-        Distribute the playbook to all teachers you support, providing video recordings, training sessions, and workshops to explain its content and implementation strategies.
+        Ensure the playbook is easily accessible and user-friendly, considering digital formats (PDFs, web pages, etc.) for distribution
+        Distribute the playbook to all teachers you support, providing video recordings, training sessions, and workshops to explain its content and implementation strategies
+        
         
         Step 5: Personalized Improvement Opportunities 
-        Use Vertex AI to quickly process and analyze individual teacher performance and student feedback.
-        Automatically generate personalized reports for each teacher, highlighting their strengths and areas for improvement based on sentiment analysis results.
+        Use Vertex AI to quickly process and analyze individual teacher performance and student feedback
+        Automatically generate personalized reports for each teacher, highlighting their strengths and areas for improvement based on sentiment analysis results
         Learn about using sentiment analysis to automate personalized report creation here
         
+        
         Step 6: Teacher Support and Continuous Improvement
-        Hold individual meetings with teachers to discuss their personalized improvement opportunities and provide guidance on implementing best practices.
-        Foster a collaborative environment where teachers can share their experiences and insights with each other.
-        Regularly update the playbook with new insights and practices based on ongoing data collection and analysis.
+        Hold individual meetings with teachers to discuss their personalized improvement opportunities and provide guidance on implementing best practices
+        Foster a collaborative environment where teachers can share their experiences and insights with each other
+        Regularly update the playbook with new insights and practices based on ongoing data collection and analysis
+        
         
         Step 7: Monitoring and Evaluation 
-        Continuously monitor the impact of the implemented best practices on student engagement, learning outcomes, and overall satisfaction.
-        Collect feedback from teachers about the effectiveness and usability of the playbook, making necessary adjustments.
-        Periodically review and refine the sentiment analysis algorithms to improve accuracy and relevance.
+        Continuously monitor the impact of the implemented best practices on student engagement, learning outcomes, and overall satisfaction
+        Collect feedback from teachers about the effectiveness and usability of the playbook, making necessary adjustments
+        Periodically review and refine the sentiment analysis algorithms to improve accuracy and relevance
+        
         
         Step 8: Reporting and Communication 
-        Create reports summarizing the improvements in teaching methods and their impact on student satisfaction and learning outcomes.
-        Share these reports with school administrators, stakeholders, and teachers to demonstrate the value of the implemented strategies.
+        Create reports summarizing the improvements in teaching methods and their impact on student satisfaction and learning outcomes
+        Share these reports with school administrators, stakeholders, and teachers to demonstrate the value of the implemented strategies
+        
         
         Step 9: Iteration and Adaptation 
-        Regularly review the effectiveness of the playbook and the sentiment analysis process.
-        Adapt the playbook and strategies as education trends, student preferences, and teaching methodologies evolve.
+        Regularly review the effectiveness of the playbook and the sentiment analysis process
+        Adapt the playbook and strategies as education trends, student preferences, and teaching methodologies evolve
 
       `;
 
@@ -140,10 +149,12 @@ io.on('connection', (socket) => {
 
   socket.on('send-evidence', async (file) => {
     const evidenceResponse = `  
-    You made the following mistakes in your documentation
+    You made the following mistakes in your documentation:
     
-    1) Lack of Negation Handling: Not accounting for negations (words like "not" or "never") can result in misclassified sentiments, as these words can reverse the sentiment orientation of a sentence.
+    1) Lack of Negation Handling: Not accounting for negations (words like "not" or "never") can result in misclassified sentiments, as these words can reverse the sentiment orientation of a sentence
+    
     2) Lack of text normalization (stemming, lemmatization)
+    
     3) There are 273 irrelevant characters, symbols, and special characters
     `;
     io.emit('response', evidenceResponse);
