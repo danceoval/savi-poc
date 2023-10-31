3befabf62ba1c4e72671cee1c97c484688732d0f
Dan Sohval <daniel.sohval@gmail.com>
2023-10-31 14:58:18 -0400

    uses drop down form for selection

--- public/react/components/QuestionForm.js
+++ public/react/components/QuestionForm.js
@@ -1,74 +1,72 @@
 import React, { useState } from 'react';
 
 export const QuestionForm = (props) => {
-  const questions = [
-    "Describe your role.",
-    "What are your current priorities?",
-    "How do you measure success?",
-    "What are the top pain points in your work?",
-    "How many hours per week do you currently spend on this pain point?",
-    "What kind of data (unstructured (e.g. user feedback) and structured (e.g. Excel spreadsheets)) do you have access to?",
-    " How do you currently use that data?"
+  const options = [
+    "I spend a lot of time manually consolidating data in various formats across disparate sources",
+    "I spent a lot of time manually summarizing a large amount of data to derive meaningful insights",
+    "I spend a lot of time performing rule-based, repetitive, highly standardized tasks"
   ];
-
-  const questionsPerPage = 8;
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [dataSources, setDataSources] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
 
-  const startIndex = (currentPage - 1) * questionsPerPage;
-  const endIndex = startIndex + questionsPerPage;
-
-  const [userAnswers, setUserAnswers] = useState(Array(questions.length)
-    .fill('')
-    .map((el, idx) => {
-      return { question: questions[idx], answer: '' };
-    }));
-
-  const handleAnswerChange = (event, questionIndex) => {
-    const updatedAnswers = [...userAnswers];
-    updatedAnswers[questionIndex].answer = event.target.value;
-    setUserAnswers(updatedAnswers);
+  const handleOptionChange = (event) => {
+    const option = event.target.value;
+    setSelectedOptions((prevOptions) => {
+      if (prevOptions.includes(option)) {
+        return prevOptions.filter((o) => o !== option);
+      } else {
+        return [...prevOptions, option];
+      }
+    });
   };
 
-  const handlePrevious = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  const handleDataSourceChange = (event, index) => {
+    const source = event.target.value;
+    setDataSources((prevSources) => {
+      const newSources = [...prevSources];
+      newSources[index] = source;
+      return newSources;
+    });
   };
 
-  const handleNext = () => {
-    setCurrentPage((prevPage) => Math.min(prevPage + 1, Math.ceil(questions.length / questionsPerPage)));
+  const handleAddDataSource = () => {
+    setDataSources([...dataSources, '']);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (userAnswers.every((answer) => answer.answer !== '')) {
+    if (selectedOptions.length > 0) {
       props.setStateIdx(1);
     } else {
-      alert("Please answer all questions before submitting.");
+      alert("Please select at least one option before submitting.");
     }
   };
 
   return (
-    <form className="question-form" onSubmit={handleSubmit}>
-      {questions.slice(startIndex, endIndex).map((question, index) => (
-        <div className="question" key={startIndex + index}>
-          <p>{question}</p>
-          <textarea
-            value={userAnswers[startIndex + index].answer}
-            onChange={(event) => handleAnswerChange(event, startIndex + index)}
-            rows={4}
-            cols={50}
-          />
+    <form className="help-form" onSubmit={handleSubmit}>
+      <div className="question">
+        <p>What do you need help with?</p>
+        <select
+          multiple
+          value={selectedOptions}
+          onChange={handleOptionChange}
+        >
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        <p>Selected Options: </p>
+        <div>
+        {
+          (selectedOptions.length > 0) && selectedOptions.map((opt, i) => <p key={i}>{opt}</p>)
+        }
         </div>
-      ))}
+      </div>
       <div className="button-container">
-        {currentPage > 1 && (
-          <button type="button" onClick={handlePrevious}>Previous</button>
-        )}
-        {currentPage < Math.ceil(questions.length / questionsPerPage) && (
-          <button type="button" onClick={handleNext}>Next</button>
-        )}
-        {currentPage === Math.ceil(questions.length / questionsPerPage) && (
-          <button type="submit">Submit</button>
-        )}
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
